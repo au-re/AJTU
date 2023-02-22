f70af222fb7b8fac6b2ba297678d62acbe66c2e9
Aurélien <au.re@live.com>
2023-02-22 19:26:57 +0100

    add sentry error handling

--- website/functions/src/index.ts
+++ website/functions/src/index.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import cors from "cors";
 import express, { Request } from "express";
 import * as functions from "firebase-functions";
@@ -21,9 +23,18 @@ import { uploadImageFromUrl } from "./utils/uploadImageFromUrl";
 
 const app = express();
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app })],
+  tracesSampleRate: 1.0,
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 /**
  * This endpoint is used create a new chapter of the story, given an action taken by the protagonist
@@ -154,6 +165,8 @@ app.get("/prompts", async (req, res) => {
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
+
+app.use(Sentry.Handlers.errorHandler());
 
 const api = functions.https.onRequest(app);
 
